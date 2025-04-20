aa1eed0152ab53313ba60ca869f0b59c9724d53c
Nyan Lin Tun <nyanlynntunn@gmail.com>
2025-04-20 22:24:18 +0800

    Check with file not found flag for raft

--- unseal-script/vault-init.sh
+++ unseal-script/vault-init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # initialize vault
-if [ ! -s /unseal-script/unseal-output.txt ]; then
+echo "Script is running from: $(pwd)"
+if [ ! -f /unseal-script/unseal-output.txt ]; then # File not found
+    echo "Vault is not initialized. Initializing now..."
     vault server -config=/vault/config/config.hcl & sleep 0.5
     vault operator init > /unseal-script/unseal-output.txt
 else
@@ -9,10 +11,12 @@ else
     echo "Cleaning up files in /vault/file/*..."
     rm -rf /vault/file/*
     vault server -config=/vault/config/config.hcl & sleep 0.5
+    echo "Raft joined successfully"
 fi
 
 # unseal vault
 for i in $(seq 1 3); do
+    echo "Unsealing Vault with Unseal Key $i"
     vault operator unseal $(grep "Unseal Key $i:" /unseal-script/unseal-output.txt | awk '{print $4}')
 done
 
@@ -23,3 +27,5 @@ echo "Unseal successfully."
 vault status
 
 vault operator raft list-peers
+
+wait
